ff1b75be19e93f966781689d8d7ad9b4ed0c1b2a
aayush1408 <aayushjaiswal0114@gmail.com>
2018-07-26 20:22:06 +0530

    intial client commit

---
+++ client/main.route.js
@@ -0,0 +1,12 @@
+myApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('');
+    $routeProvider.
+        when('/', {
+            templateUrl: 'views/home.html',
+        })
+        .when('/display', {
+            templateUrl: 'views/display.html',
+        })
+        .otherwise({ redirectTo: '/' })
+}]);
