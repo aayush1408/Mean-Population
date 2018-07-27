ee6753a653fa31a38df22c6980f30879b95ff68e
aayush1408 <aayushjaiswal0114@gmail.com>
2018-07-27 16:30:30 +0530

    add and updated method added

--- client/controllers/display.js
+++ client/controllers/display.js
@@ -1,6 +1,7 @@
 myApp.controller('displayController', ['$scope', '$http', function ($scope, $http) {
     $scope.population = []
-
+    $scope.isEdit = false
+    $scope.isAdd = false
     $scope.displayValues = function () {
         $http.get("http://localhost:5000/api/display").then(function (response) {
             console.log(response.data)
@@ -23,5 +24,60 @@ myApp.controller('displayController', ['$scope', '$http', function ($scope, $htt
             console.log(err)
         })
     }
+    $scope.editData = function (pop) {
+        $scope.isEdit = true
+        $scope.editId = pop._id
+        $scope.editYear = pop.year
+        $scope.editPopulation = pop.population
+        $scope.editGrowth = pop.growth
+        $scope.editRate = pop.rate
+    }
 
+    $scope.updateData = function () {
+        console.log($scope.editId)
+        let newData = {
+            _id: $scope.editId,
+            year: $scope.editYear,
+            population: $scope.editPopulation,
+            growth: $scope.editGrowth,
+            rate: $scope.editRate
+        }
+        console.log(newData)
+        $http({
+            method: 'PUT',
+            url: 'http://localhost:5000/api/edit/' + $scope.editId,
+            headers: { "Content-Type": "application/json;charset=UTF-8" },
+            data: newData
+        }).then(function (response) {
+            console.log(response)
+            alert('Updated')
+            $scope.displayValues()
+        }).catch(function (err) {
+            console.log(err)
+        })
+    }
+    $scope.addForm = function () {
+        $scope.isAdd = true
+    }
+
+    $scope.addData = function () {
+        let newData = {
+            year: $scope.addYear,
+            population: $scope.addPopulation,
+            growth: $scope.addGrowth,
+            rate: $scope.addRate
+        }
+        $http({
+            method: 'POST',
+            url: 'http://localhost:5000/api/add/',
+            headers: { "Content-Type": "application/json;charset=UTF-8" },
+            data: newData
+        }).then(function (response) {
+            console.log(response)
+            alert('Added')
+            $scope.displayValues()
+        }).catch(function (err) {
+            console.log(err)
+        })
+    }
 }])
